cf7d013b1c2b5c0becb143004efe3c88eacfe596
brandonsladek <brandonsladek@gmail.com>
2015-02-21 21:56:58 -0700

    Final product

---
+++ BinarySearchTree.java
@@ -0,0 +1,80 @@
+package csci232.lab2;
+
+public class BinarySearchTree {
+
+	// Nested class to create nodes for the tree
+	public class Node {
+		
+		// Every Node consists of an integer element and most have links to their parent and children nodes
+		int element;
+		Node parent;
+		Node leftChild;
+		Node rightChild;
+		
+		// Get methods
+		public int getElement() { return element; }
+		public Node getParent() { return parent; }
+		public Node getLeftChild() { return leftChild; }
+		public Node getRightChild() { return rightChild; }
+		
+		// Set methods
+		public void setElement(int num) { element = num; }
+		public void setParent(Node p) { parent = p; }
+		public void setLeftChild(Node l) { leftChild = l; }
+		public void setRightChild(Node r) { rightChild = r; }
+		
+		// Constructor for Node 
+		public Node(int e, Node p, Node l, Node r) {
+			element = e;
+			parent = p;
+			leftChild = l;
+			rightChild = r;
+		}
+		
+	} // End of nested Node class
+	
+	// Insert the argument node into the tree using root
+	public void insertNode(Node node, Node root) { 
+		
+		Node current = root;
+        Node previous = null;
+        
+        // Node integer value
+        int number = node.getElement();
+
+        // Traverse tree to determine where node should be inserted
+        while (current != null) {
+            previous = current;
+            if (number <= current.getElement()) {
+                current = current.getLeftChild();
+            }
+            else {
+                current = current.getRightChild();
+            }
+        }
+        
+        // Determine whether new node should be leftChild or rightChild
+        if (number <= previous.getElement()) {
+            previous.setLeftChild(node);
+            node.setParent(previous);
+        }
+        else {
+            previous.setRightChild(node);
+            node.setParent(previous);
+        }
+    } // End of insertNode method 
+	
+	public void inorderTraversal(Node root) {
+		if (root.getLeftChild() != null) {
+			inorderTraversal(root.getLeftChild());
+		}
+			System.out.print(root.getElement() + " ");
+		if (root.getRightChild() != null) {
+			inorderTraversal(root.getRightChild());
+		}	
+	} // End of inorderTraversal method
+	
+	// Traverse tree to determine if node is present
+	public void searchTree(Node root) { }
+	
+} // End of BinarySearchTree class
--- HashTable.java
+++ HashTable.java
@@ -1,34 +1,42 @@
 package csci232.lab2;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import csci232.lab2.BinaryTree.Node;
+import csci232.lab2.BinarySearchTree.Node;
 
 public class HashTable {
 
+	static Node nodeAtIndex;
 	static Node[] hashTable;
 	static int sizeHashTable;
-	static BinaryTree bt = new BinaryTree();
+	static int numInsertedElements;
+	static BinarySearchTree bst = new BinarySearchTree();
 	
 	// Calculate the index where the element should be inserted into the hash table
 	public static int hashFunction(int numToInsert) {
 		int index;
-		index = numToInsert % sizeHashTable;
+		index = numToInsert % (sizeHashTable);
 		return index;
 	} // End of hashFunction method
 	
 	// Insert an element into the hash table
 	public static void insertElement(int numToInsert) {
+		
+		// Use hash function to find index
 		int index = hashFunction(numToInsert);
-		Node newNode = bt.new Node(numToInsert, null, null, null);
+		nodeAtIndex = hashTable[index];
+		
+		Node newNode = bst.new Node(numToInsert, null, null, null);
 		
 		// If there is no collision
 		if (hashTable[index] == null) {
-		hashTable[index] = newNode;
-		} 
-		// If there is a collision, insert into binary tree at collision index
-			else {
-			
+			hashTable[index] = newNode;
+		}  
+		// If there is a collision, use the nodeAtIndex as root to insert the node into the binary search tree
+		else {
+			bst.insertNode(newNode, nodeAtIndex);
 		}
 	} // End of insertElement method
 	
@@ -36,38 +44,98 @@ public class HashTable {
 	public static void main(String[] args) {
 		
 		int numToInsert;
-		String quit = "continue";
+		String input = null;
 		
-		Scanner in = new Scanner(System.in);
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println("Welcome to the HashBinary program.");
+		System.out.println("Welcome to the HashBST program.");
 		System.out.println();
-		System.out.print("Enter the size of the hash table you would like to work with >>> ");
+		System.out.print("Enter the integer size of the hash table you would like to work with >>> ");
 		
-		sizeHashTable = in.nextInt();
-		System.out.println();
-		
-		hashTable = new Node[sizeHashTable];
-		
-		System.out.println("A hash table of size " + sizeHashTable + " has been created.");
+		// Begin reading in user input
+		try {
+			input = buffer.readLine();
+			
+			// If input is an integer
+			if (isInt(input)) {
+				sizeHashTable = Integer.parseInt(input);
+				hashTable = new Node[sizeHashTable];
+				System.out.println();
+				System.out.println("A hash table of size " + sizeHashTable + " has been created.");
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		// Repeatedly ask the user to enter an integer into the hash table, quit if user enters q
-		while(!quit.equals("q")) {
+		while(!input.equals("q")) {
 		
 			System.out.println();
 			System.out.println("To insert an integer into the hash table, enter an integer.");
-			System.out.println("To quit, enter q");
+			System.out.println("To quit entering values into the hash table, enter q");
 		
 			// Get user input
-			numToInsert = in.nextInt();
-			quit = in.nextLine();
+			
 			System.out.println();
+			
+			try {
+				input = buffer.readLine();
+				
+				if (isInt(input)) {
+					numToInsert = Integer.parseInt(input);
+					
+					// Insert the integer into the hashTable
+					insertElement(numToInsert);
+					numInsertedElements++;
+			
+					System.out.println();
+					System.out.println("The number " + numToInsert + " has been added to the hash table.");
+					System.out.println();
+				} else {
+					input = "q";
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			} // End of try-catch statement
+			
+		} // End while loop
 		
-			insertElement(numToInsert);
-			System.out.println("The number " + numToInsert + " has been added to the hash table.");
-			System.out.println();
-		}
+		System.out.println();
+		System.out.println("Would you like to display an inorder traversal of a tree at a specific index? (yes/no)");
 		
-	} // End of main method
+		// Read in user input
+		try {
+			input = buffer.readLine();
+			
+			if (input.equals("yes")) {
+				System.out.println();
+				System.out.println("Enter the index value. NOTE: It must be less than " + sizeHashTable + ".");
+				System.out.println("NOTE: The hash function being used is (x % sizeHashTable), so if ");
+				System.out.println("x % 5 = 3, the value x will be placed in index 3, rather than 2.");
+				input = buffer.readLine();
+				int index = Integer.parseInt(input);
+				
+				// Print out an in order traversal of the tree at the entered index of hash table
+				System.out.println();
+				bst.inorderTraversal(hashTable[index]);
+				System.out.println();
+			} // End if
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // End of try-catch statement
+		
+		// Good manners
+		System.out.println();
+		System.out.println("Goodbye.");
+		
+	} // End main method
+
+	// Use regular expression to check whether the user input is an integer
+	public static boolean isInt(String check) {
+		return check.matches("\\d+");
+	} // End of isInt method
 	
 } // End of HashTable class
